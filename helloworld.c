e6a790a182120e96ce21fb41bb0e7989fc331e94
i92guloj <64157406+i92guloj@users.noreply.github.com>
2020-04-22 19:02:37 +0200

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+//Author: i92 guloj; Date: abril 2020
+=======
+//merge con conflictos
+>>>>>>> branchA
+
+#include "./include/myinclude.h"
+
+int main(){
+	f();
+	return 0;
+}
---
+++ include/myinclude.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+void f()
+{
+	char c1[100]= "Hola mundo";
+	char c2[100]= ", I am i92guloj";
+	printf("%s\n", strcat(c1, c2));
+}
